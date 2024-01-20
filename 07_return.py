4d02314953e778f434e107c8c1da237fd2e6ddae
Robert Vari <mail.robertvari@gmail.com>
2024-01-20 15:38:46 +0100

    update

---
+++ 07_return.py
@@ -0,0 +1,17 @@
+def add_numbers(a, b):
+    return a + b
+
+
+def divide_numbers(a, b):
+    return a / b
+
+
+def multiply_numbers(a, b):
+    return a * b
+
+
+add_result = add_numbers(24, 20)
+divide_resoult = divide_numbers(add_result, 20)
+multiply_result = multiply_numbers(divide_resoult, 200)
+
+pass
