f46f04b3a78400650977bd8c4c47de0dfa7828cc
Robert Vari <mail.robertvari@gmail.com>
2024-01-20 14:23:45 +0100

    update

---
+++ coffeemake.py
@@ -0,0 +1,16 @@
+import time
+
+def coffeemaker():
+    print("-"*50)
+    print("Coffee maker is started...")
+    time.sleep(2)
+    print("Water is boiling...")
+    time.sleep(3)
+    print("Your coffee is ready.")
+    print("Enjoy!")
+    print("-"*50)
+
+
+coffeemaker()
+coffeemaker()
+coffeemaker()
