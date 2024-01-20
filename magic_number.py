6cba6813d9d8a2c1d2358db2c216adba1d7c5ee6
Robert Vari <mail.robertvari@gmail.com>
2024-01-20 16:58:30 +0100

    update

---
+++ magic_number.py
@@ -0,0 +1,29 @@
+import os
+
+# entry point
+def main():
+    pass
+
+
+# A clear terminal 
+def clear_screen():
+    pass
+
+# Print game intro
+def intro():
+    pass
+
+
+# Ask for the player name and store it in a global variable
+def get_player_name():
+    pass
+
+
+# The Game Loop
+def game():
+    pass
+
+
+# Ask for the player if he/she wants a new turn.
+def ask_new_game():
+    pass
