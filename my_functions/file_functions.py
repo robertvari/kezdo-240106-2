e7b368ace053b85e4763de3644a81051f3cbbc6d
Robert Vari <mail.robertvari@gmail.com>
2024-01-21 15:47:32 +0100

    update

--- my_functions/file_functions.py
+++ my_functions/file_functions.py
@@ -16,4 +16,17 @@ def get_all_files(root_folder: str, file_list: list):
     # check is file_ist is a list
     assert isinstance(file_list, list), "file_list must be of type list"
 
-    
+    root_folder_content = os.listdir(root_folder)
+
+    subfolders = []
+
+    # collect files and subfolders
+    for i in root_folder_content:
+        full_path = os.path.join(root_folder, i)
+        if os.path.isfile(full_path):
+            file_list.append(i)
+        else:
+            subfolders.append(i)
+
+    for folder in subfolders:
+        get_all_files(folder, file_list)
