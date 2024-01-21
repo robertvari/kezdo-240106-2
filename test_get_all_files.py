fd2a8345bb9250b3a061c0a2ae4e904510311e30
Robert Vari <mail.robertvari@gmail.com>
2024-01-21 16:11:11 +0100

    update

--- test_get_all_files.py
+++ test_get_all_files.py
@@ -1,8 +1,8 @@
 from my_functions.file_functions import get_all_files
 
-root_folder = r"D:\Work\_PythonSuli\kezdo-240106"
+root_folder = r"D:\Work\_PythonSuli\kezdo-240106\get_files_test"
 file_list = []
-get_all_files(root_folder, file_list, extension=".py")
+get_all_files(root_folder, file_list, extension=".jpg")
 
 for i in file_list:
     print(i)
